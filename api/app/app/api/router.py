9c5b4f4dd8ddfab8b44b062c5d559503629a3421
martian56 <fuadelizade469@gmail.com>
2026-04-19 19:31:02 +0400

    feat(api): mount public board and templates routers

--- api/app/app/api/router.py
+++ api/app/app/api/router.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
 from app.modules.auth.router import router as auth_router
+from app.modules.boards.router import public_router as boards_public_router
 from app.modules.boards.router import router as boards_router
+from app.modules.boards.router import templates_router as boards_templates_router
 from app.modules.elements.router import router as elements_router
 from app.modules.users.router import router as users_router
 from app.modules.workspaces.router import router as workspaces_router
@@ -12,5 +14,7 @@ api_router.include_router(auth_router)
 api_router.include_router(users_router)
 api_router.include_router(workspaces_router)
 api_router.include_router(boards_router)
+api_router.include_router(boards_public_router)
+api_router.include_router(boards_templates_router)
 api_router.include_router(elements_router)
 api_router.include_router(ws_router)
