5d0eeea219d4bb09776af45fbf70bc7edaed2c4f
martian56 <fuadelizade469@gmail.com>
2026-04-19 19:31:23 +0400

    feat(ui): export new auth pages

--- apps/frontend/src/pages/auth/index.ts
+++ apps/frontend/src/pages/auth/index.ts
@@ -1,4 +1,7 @@
 export { AuthCallbackPage } from "./AuthCallbackPage";
+export { ForgotPasswordPage } from "./ForgotPasswordPage";
 export { InvitePage } from "./InvitePage";
 export { LoginPage } from "./LoginPage";
 export { RegisterPage } from "./RegisterPage";
+export { ResetPasswordPage } from "./ResetPasswordPage";
+export { VerifyEmailPage } from "./VerifyEmailPage";
